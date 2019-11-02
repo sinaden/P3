461df67ce9697db791161e63a82711033fa65a13
sinaCopenhagen123 <sinadenmark@gmail.com>
2019-11-02 11:53:44 +0100

    Buttons for base fragment added

--- app/src/main/java/com/example/p3/BaseFragment.java
+++ app/src/main/java/com/example/p3/BaseFragment.java
@@ -16,6 +16,10 @@ public class BaseFragment extends Fragment {
     private static final String TAG = "BaseFragment";
 
     private ImageView btnRoom;
+    private ImageView btnGlobal;
+    private ImageView btnRoulette;
+    private ImageView btnFriends;
+    private ImageView btnSettings;
 
     @Nullable
     @Override
@@ -23,12 +27,42 @@ public class BaseFragment extends Fragment {
         Log.d(TAG, "started");
         View view = inflater.inflate(R.layout.activity_main, container, false);
         btnRoom = (ImageView) view.findViewById(R.id.chat_rooms);
-
+        btnGlobal = (ImageView) view.findViewById(R.id.global_chat);
+        btnRoulette = (ImageView) view.findViewById(R.id.chat_roulette);
+        btnFriends = (ImageView) view.findViewById(R.id.friends);
+        btnSettings = (ImageView) view.findViewById(R.id.settings);
 
         btnRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Clicked on chatroombtn");
+                Log.d(TAG, "Clicked on Rooms");
+            }
+        });
+
+        btnGlobal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clicked on Glob");
+            }
+        });
+
+        btnRoulette.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clicked on Rou");
+            }
+        });
+        btnFriends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clicked on Fre");
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clicked on Set");
             }
         });
 
